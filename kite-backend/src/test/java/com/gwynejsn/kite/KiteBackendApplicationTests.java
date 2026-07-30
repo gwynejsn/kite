1a53babdd9071294dc607c83973fb59fbc554855
Gwyne Justin Deterala <gwynejustin.deterala.p@gmail.com>
2026-07-30 11:18:40 +0800

    feat: initialize Spring Boot backend for kite

---
+++ kite-backend/src/test/java/com/gwynejsn/kite/KiteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gwynejsn.kite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiteBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
