2cb1a962ddf7049c56956d90a6710e2c303e1e0c
Gwyne Justin Deterala <gwynejustin.deterala.p@gmail.com>
2026-08-20 13:05:12 +0800

    android support fixed.

--- kite-flutter/kite/android/app/build.gradle.kts
+++ kite-flutter/kite/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.kite"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
     buildToolsVersion = "36.0.0"
 
